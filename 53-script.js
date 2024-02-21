eb5e49887f0cd8f486146d47ecb8b8e45d912534
Paulo19961944 <paulo24041996@gmail.com>
2024-02-21 14:39:00 -0300

    Curso Web Designer - CSS | V1.7.8

--- 53-script.js
+++ 53-script.js
@@ -1,22 +1,28 @@
-const navbarNavigation = document.querySelector(".navbar-navigation");
-        const navBtn = document.querySelector(".nav-btn");
-        const navbarContent = document.querySelector(".navbar-content");
-        navBtn.addEventListener("click", () => {
-            navbarContent.style.display = "flex";
-            navbarContent.style.flexDirection = "column";
-            navbarNavigation.style.alignItems = "center"
-            navbarNavigation.style.justifyContent = "center"
-            navBtn.style.display = "none";
-        });
+// Entradas HTML
+const navbarNavigation = document.querySelector(".navbar-navigation"); // Barra de Navegação Inteira
+const navBtn = document.querySelector(".nav-btn"); // Menu de Navegação
+const navbarContent = document.querySelector(".navbar-content"); //  Conteúdo da Barra de Navegação
 
-        document.addEventListener("click", (event) => {
-            const isClickedonMenu = navbarContent.contains(event.target);
-            const isClickedOnMenuIcon = navBtn.contains(event.target);
-            if (!isClickedonMenu && !isClickedOnMenuIcon) {
-                navbarContent.style.display = "";
-                navbarContent.style.flexDirection = "";
-                navbarNavigation.style.alignItems = ""
-                navbarNavigation.style.justifyContent = ""
-                navBtn.style.display = "";
-            }
-        });
+// Evento ao Clicar no Botão de Menu
+navBtn.addEventListener("click", () => {
+    navbarContent.style.display = "flex"; // Flexbox
+    navbarContent.style.flexDirection = "column"; // Alinha em Colunas
+    navbarNavigation.style.alignItems = "center" // Alinha ao Centro
+    navbarNavigation.style.justifyContent = "center" //Alinha ao Centro
+    navBtn.style.display = "none"; // Esconde o Menu de Botão
+});
+
+//Adiciona Evento ao Clicar Fora do Botão de Menu
+document.addEventListener("click", (event) => {
+    const isClickedonMenu = navbarContent.contains(event.target); // Verifica se Clicou no Menu
+    const isClickedOnMenuIcon = navBtn.contains(event.target); //Verifica se Clicou no Botão de Menu
+
+    // Se Não Clicou no Menu ou no Botão, então reseta o Padrão Original
+    if (!isClickedonMenu && !isClickedOnMenuIcon) {
+        navbarContent.style.display = ""; // Reseta o Padrão Original
+        navbarContent.style.flexDirection = ""; // Reseta o Padrão Original
+        navbarNavigation.style.alignItems = "" // Reseta o Padrão Original
+        navbarNavigation.style.justifyContent = "" // Reseta o Padrão Original
+        navBtn.style.display = ""; // Reseta o Padrão Original
+    }
+});
